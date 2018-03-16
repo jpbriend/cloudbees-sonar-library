befbaa39eb3740e057c978055e7540bbc2d87979
Jean-Philippe Briend <jeanphilippe.briend@gmail.com>
2018-03-16 14:33:18 +0100

    Remove stage

--- vars/analyseSonar.groovy
+++ vars/analyseSonar.groovy
@@ -1,45 +1,12 @@
-def call( Closure body) {
-    stage('Build master') {
-      agent {
-        label 'linux'
-      }         
-      when {
+def call() {
+    when {
         branch 'master'
-      }
-      steps {
-        withSonarQubeEnv('next') {
-          withMaven(maven: MAVEN_TOOL) {
-            sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar"
-            
-          }
-        }
-      }
     }
-    stage('Build maintenance branch') {
-      when {
-        branch 'branch-*'
-      }
-      steps {
+    steps {
         withSonarQubeEnv('next') {
-          withMaven(maven: MAVEN_TOOL) {
-            // sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.branch.name=${env.BRANCH_NAME}"
-          }
+            withMaven(maven: MAVEN_TOOL) {
+            sh "mvn org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.branch=${env.BRANCH_NAME}"
+            }
         }
-      }
-    }
-    stage('Build PR'){
-      when {
-        branch 'PR-*'
-      }
-      steps {
-        withSonarQubeEnv('next') {
-          withMaven(maven: MAVEN_TOOL) {
-            sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
-            -Dsonar.pullrequest.branch=${env.BRANCH_NAME} \
-            -Dsonar.pullrequest.base='master' \
-            -Dsonar.pullrequest.key=${env.CHANGE_ID}"                  
-          }
-        }
-      }
     }
 }
